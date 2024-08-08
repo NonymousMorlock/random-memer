fc8ee3faaa18deb0ef4b3865b89db7038b5b605d
NonymousMorlock <agamspaul@gmail.com>
2024-08-08 02:57:16 +0000

    Update scraper

--- app.py
+++ app.py
@@ -4,15 +4,32 @@
 @Author  : Tushar Mittal (chiragmittal.mittal@gmail.com)
 Flask API to return random meme images
 """
-
+import os
 import random
-import requests
-from bs4 import BeautifulSoup
-from flask import Flask, send_file
-from PIL import Image
 from io import BytesIO
 
+import requests
+from PIL import Image
+from bs4 import BeautifulSoup
+from flask import Flask, send_file
+
 app = Flask(__name__)
+
+
+def get_new_memes_api():
+    url = "https://programming-memes-images.p.rapidapi.com/v1/memes"
+
+    headers = {
+        "x-rapidapi-key": os.environ['RAPIDAPI_KEY'],
+        "x-rapidapi-host": "programming-memes-images.p.rapidapi.com"
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    memes = response.json()
+    images = [meme['image'] for meme in memes]
+
+    return images
+
 
 def get_new_memes():
     """Scrapers the website and extracts image URLs
@@ -20,15 +37,21 @@ def get_new_memes():
     Returns:
         imgs [list]: List of image URLs
     """
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
+        "Cookie": "PHPSESSID=44ffmkgnot4ihuopf8ot05s1s3; _ga=GA1.2.2105294567.1668857619; "
+                  "_gid=GA1.2.1610244212.1668857619; _gat=1",
+        'Accept-Language': 'en-US,en;q=0.5'
+    }
     url = 'https://www.memedroid.com/memes/tag/programming'
-    response = requests.get(url)
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
     soup = BeautifulSoup(response.content, 'lxml')
-    divs = soup.find_all('div', class_='item-aux-container')
+    pics = soup.find_all('picture')
     imgs = []
-    for div in divs:
-        img = div.find('img')['src']
-        if img.startswith('http') and img.endswith('jpeg'):
-            imgs.append(img)
+    for pic in pics:
+        img = pic.find('img')
+        imgs.append(img['src'])
     return imgs
 
 
@@ -47,6 +70,7 @@ def serve_pil_image(pil_img):
     img_io.seek(0)
     return send_file(img_io, mimetype='image/jpeg')
 
+
 @app.after_request
 def set_response_headers(response):
     """Sets Cache-Control header to no-cache so GitHub
@@ -57,6 +81,7 @@ def set_response_headers(response):
     response.headers['Expires'] = '0'
     return response
 
+
 @app.route("/", methods=['GET'])
 def return_meme():
     img_url = random.choice(get_new_memes())
@@ -64,3 +89,7 @@ def return_meme():
     res.raw.decode_content = True
     img = Image.open(res.raw)
     return serve_pil_image(img)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
